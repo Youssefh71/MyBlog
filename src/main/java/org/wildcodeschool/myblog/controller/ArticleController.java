306683c1e65cb72db01bc0fb984f65b13b3ca0c7
Youssefh71 <youssefhourri69@gmail.com>
2025-02-10 14:17:36 +0100

    Spring Spring 9 - Service

--- src/main/java/org/wildcodeschool/myblog/controller/ArticleController.java
+++ src/main/java/org/wildcodeschool/myblog/controller/ArticleController.java
@@ -10,6 +10,7 @@ import org.wildcodeschool.myblog.exception.ResourceNotFoundException;
 import org.wildcodeschool.myblog.mapper.ArticleMapper;
 import org.wildcodeschool.myblog.model.*;
 import org.wildcodeschool.myblog.repository.*;
+import org.wildcodeschool.myblog.service.ArticleService;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -20,244 +21,70 @@ import java.util.stream.Collectors;
 @RequestMapping("/articles")
 public class ArticleController {
 
-
-    private final ArticleRepository articleRepository;
-    private final CategoryRepository categoryRepository;
-    private final ImageRepository imageRepository;
-    private final AuthorRepository authorRepository;
-    private final ArticleAuthorRepository articleAuthorRepository;
-    private final ArticleMapper articleMapper;
+    private final ArticleService articleService;
 
 
-    public ArticleController(ArticleRepository articleRepository, CategoryRepository categoryRepository, ImageRepository imageRepository, AuthorRepository authorRepository, ArticleAuthorRepository articleAuthorRepository, ArticleMapper articleMapper) {
-        this.articleRepository = articleRepository;
-        this.categoryRepository = categoryRepository;
-        this.imageRepository = imageRepository;
-        this.authorRepository = authorRepository;
-        this.articleAuthorRepository = articleAuthorRepository;
-        this.articleMapper = articleMapper;
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
     }
-
 
     @GetMapping
     public ResponseEntity<List<ArticleDTO>> getAllArticles() {
-                List<ArticleDTO> articleDTOS = articleRepository.findAll()
-                        .stream()
-                        .map(articleMapper::toDTO).toList();
-        return articleDTOS.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(articleDTOS);
+                List<ArticleDTO> articles = articleService.getAllArticles();
+        return ResponseEntity.ok(articles);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ArticleDTO> getArticleById(@PathVariable Long id) {
-        Article article = articleRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("L'article avec l'id " + id + " n'a pas été trouvé"));
-        if (article == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(convertToDTO(article));
+        ArticleDTO article = articleService.getArticleById(id);
+        return (article == null) ? ResponseEntity.notFound().build() :
+                ResponseEntity.ok(article);
     }
 
     @PostMapping
     public ResponseEntity<ArticleDTO> createArticle(@RequestBody Article article) {
-
-        LocalDateTime now = LocalDateTime.now();
-        article.setCreatedAt(now);
-        article.setUpdatedAt(now);
-
-        if (article.getCategory() != null) {
-            Category category = categoryRepository.findById(article.getCategory().getId()).orElseThrow(() -> new ResourceNotFoundException("La catégorie avec l'id " + article.getCategory().getId() + " n'a pas été trouvée"));
-            if (category == null) {
-                return ResponseEntity.badRequest().build();
-            }
-            article.setCategory(category);
-        }
-
-        if (article.getImages() != null && !article.getImages().isEmpty()) {
-            List<Image> validImages = new ArrayList<>();
-            for (Image image : article.getImages()) {
-                if (image.getId() != null) {
-                    // Vérification des images existantes
-                    Image existingImage = imageRepository.findById(image.getId()).orElse(null);
-                    if (existingImage != null) {
-                        validImages.add(existingImage);
-                    } else {
-                        return ResponseEntity.badRequest().body(null);
-                    }
-                } else {
-                    // Création de nouvelles images
-                    Image savedImage = imageRepository.save(image);
-                    validImages.add(savedImage);
-                }
-            }
-            article.setImages(validImages);
-        }
-        Article savedArticle = articleRepository.save(article);
-        if (article.getArticleAuthors() != null) {
-            for (ArticleAuthor articleAuthor : article.getArticleAuthors()) {
-                Author author = articleAuthor.getAuthor();
-                author = authorRepository.findById(author.getId()).orElse(null);
-                if (author == null) {
-                    return ResponseEntity.badRequest().body(null);
-                }
-
-                articleAuthor.setAuthor(author);
-                articleAuthor.setArticle(savedArticle);
-                articleAuthor.setContribution(articleAuthor.getContribution());
-
-                articleAuthorRepository.save(articleAuthor);
-            }
-        }
-        return ResponseEntity.status(HttpStatus.CREATED).body(convertToDTO(savedArticle));
+        ArticleDTO savedArticle = articleService.createArticle(article);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<ArticleDTO> updateArticle(@PathVariable Long id, @RequestBody Article articleDetails) {
-        Article article = articleRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("L'article avec l'id " + id + " n'a pas été trouvé"));
-        if (article == null) {
-            return ResponseEntity.notFound().build();
-        }
-        article.setTitle(articleDetails.getTitle());
-        article.setContent(articleDetails.getContent());
-        article.setUpdatedAt(LocalDateTime.now());
-
-        if (articleDetails.getCategory() != null) {
-            Category category = categoryRepository.findById(articleDetails.getCategory().getId()).orElseThrow(() -> new ResourceNotFoundException("L'article avec l'id " + id + " n'a pas été trouvé"));
-            if (category == null) {
-                return ResponseEntity.badRequest().build();
-            }
-            article.setCategory(category);
-        }
-
-        if (articleDetails.getImages() != null) {
-            List<Image> validImages = new ArrayList<>();
-            for (Image image : articleDetails.getImages()) {
-                if (image.getId() != null) {
-                    // Vérification des images existantes
-                    Image existingImage = imageRepository.findById(image.getId()).orElseThrow(() -> new ResourceNotFoundException("L'article avec l'id " + id + " n'a pas été trouvé"));
-                    if (existingImage != null) {
-                        validImages.add(existingImage);
-                    } else {
-                        return ResponseEntity.badRequest().build(); // Image non trouvée, retour d'une erreur
-                    }
-                } else {
-                    // Création de nouvelles images
-                    Image savedImage = imageRepository.save(image);
-                    validImages.add(savedImage);
-                }
-            }
-            // Mettre à jour la liste des images associées
-            article.setImages(validImages);
-        } else {
-            // Si aucune image n'est fournie, on nettoie la liste des images associées
-            article.getImages().clear();
-        }
-        if (articleDetails.getArticleAuthors() != null) {
-            articleAuthorRepository.deleteAll(article.getArticleAuthors());
-
-            List<ArticleAuthor> updatedArticleAuthors = new ArrayList<>();
-
-            for (ArticleAuthor articleAuthorDetails : articleDetails.getArticleAuthors()) {
-                Author author = articleAuthorDetails.getAuthor();
-                author = authorRepository.findById(author.getId()).orElseThrow(() -> new ResourceNotFoundException("L'article avec l'id " + id + " n'a pas été trouvé"));
-                if (author == null) {
-                    return ResponseEntity.badRequest().build();
-                }
-
-                ArticleAuthor newArticleAuthor = new ArticleAuthor();
-                newArticleAuthor.setAuthor(author);
-                newArticleAuthor.setArticle(article);
-                newArticleAuthor.setContribution(articleAuthorDetails.getContribution());
-
-                updatedArticleAuthors.add(newArticleAuthor);
-            }
-
-            articleAuthorRepository.saveAll(updatedArticleAuthors);
-
-            article.setArticleAuthors(updatedArticleAuthors);
-        }
-
-
-        Article updatedArticle = articleRepository.save(article);
-        return ResponseEntity.ok(convertToDTO(updatedArticle));
+        ArticleDTO updatedArticle = articleService.updateArticle(id, articleDetails);
+        return (updatedArticle != null) ? ResponseEntity.ok(updatedArticle) :
+                ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Article> deleteArticle(@PathVariable Long id) {
-        Article article = articleRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("L'article avec l'id " + id + " n'a pas été trouvé"));
-        if (article == null) {
-            return ResponseEntity.notFound().build();
-        }
-        if (article.getArticleAuthors() != null) {
-            articleAuthorRepository.deleteAll(article.getArticleAuthors());
-        }
-        articleRepository.delete(article);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<Void> deleteArticle(@PathVariable Long id) {
+        return articleService.deleteArticle(id) ? ResponseEntity.noContent().build()
+                : ResponseEntity.notFound().build();
     }
 
     @GetMapping("/search-title")
     public ResponseEntity<List<ArticleDTO>> getArticlesByTitle(@RequestParam String searchTerms){
-        List<Article> articles = articleRepository.findByTitle(searchTerms);
-        if (articles.isEmpty()) {
-            return ResponseEntity.noContent().build();
-        }
-        List<ArticleDTO> articleDTOS = articles.stream().map(this::convertToDTO).collect(Collectors.toList());
-        return ResponseEntity.ok(articleDTOS);
+        List<ArticleDTO> articles = articleService.getArticlesByTitle(searchTerms);
+        return ResponseEntity.ok(articles);
     }
 
     @GetMapping("/search")
     public ResponseEntity<List<ArticleDTO>> getArticlesByContent(@RequestParam String keyword){
-        List<Article> articlesByContent = articleRepository.findByContentContainingIgnoreCase(keyword);
-        if (articlesByContent.isEmpty()) {
-            return ResponseEntity.noContent().build();
-        }
-        List<ArticleDTO> articleDTOS = articlesByContent.stream().map(this::convertToDTO).collect(Collectors.toList());
-        return ResponseEntity.ok(articleDTOS);
+        List<ArticleDTO> articlesByContent = articleService.getArticlesByContent(keyword);
+        return (articlesByContent.isEmpty()) ? ResponseEntity.noContent().build():
+                ResponseEntity.ok(articlesByContent);
     }
 
     @GetMapping("/created-after")
     public ResponseEntity<List<ArticleDTO>> getArticlesByCreatedAfter(@RequestParam LocalDateTime createdAfter){
-        List<Article> articlesByCreatedAfter = articleRepository.findByCreatedAtAfter(createdAfter);
-        if (articlesByCreatedAfter.isEmpty()) {
-            return ResponseEntity.noContent().build();
-        }
-        List<ArticleDTO> articleDTOS = articlesByCreatedAfter.stream().map(this::convertToDTO).collect(Collectors.toList());
-        return ResponseEntity.ok(articleDTOS);
+        List<ArticleDTO> articlesByCreatedAfter = articleService.getArticlesByCreatedAfter(createdAfter);
+       return (articlesByCreatedAfter.isEmpty()) ? ResponseEntity.noContent().build() :
+               ResponseEntity.ok(articlesByCreatedAfter);
     }
 
     @GetMapping("/order")
     public ResponseEntity<List<Article>> getFiveLastArticles(){
-        List<Article> fiveLastArticles = articleRepository.findByOrderByCreatedAtDesc(Limit.of(5));
-        if (fiveLastArticles.isEmpty()) {
-            return (ResponseEntity.noContent().build());
-
-        }
-        return ResponseEntity.ok(fiveLastArticles);
-    }
-
-    private ArticleDTO convertToDTO(Article article) {
-        ArticleDTO articleDTO = new ArticleDTO();
-        articleDTO.setId(article.getId());
-        articleDTO.setTitle(article.getTitle());
-        articleDTO.setContent(article.getContent());
-        articleDTO.setUpdatedAt(article.getUpdatedAt());
-        if (article.getCategory() != null) {
-            articleDTO.setCategoryName(article.getCategory().getName());
-        }
-        if (article.getImages() != null) {
-            articleDTO.setImageUrls(article.getImages().stream().map(Image::getUrl).collect(Collectors.toList()));
-        }
-        if (article.getArticleAuthors() != null) {
-            articleDTO.setAuthors(article.getArticleAuthors().stream()
-                    .filter(articleAuthor -> articleAuthor.getAuthor() != null)
-                    .map(articleAuthor -> {
-                        AuthorDTO authorDTO = new AuthorDTO();
-                        authorDTO.setId(articleAuthor.getAuthor().getId());
-                        authorDTO.setFirstname(articleAuthor.getAuthor().getFirstname());
-                        authorDTO.setLastname(articleAuthor.getAuthor().getLastname());
-                        return authorDTO;
-                    })
-                    .collect(Collectors.toList()));
-        }
-        return articleDTO;
+        List<Article> fiveLastArticles = articleService.getFiveLastArticles();
+        return (fiveLastArticles.isEmpty()) ? (ResponseEntity.noContent().build()) :
+                ResponseEntity.ok(fiveLastArticles);
     }
 
 }
---
+++ src/main/java/org/wildcodeschool/myblog/service/ArticleService.java
@@ -0,0 +1,210 @@
+package org.wildcodeschool.myblog.service;
+
+import org.springframework.data.domain.Limit;
+import org.springframework.stereotype.Service;
+import org.wildcodeschool.myblog.dto.ArticleDTO;
+import org.wildcodeschool.myblog.exception.ResourceNotFoundException;
+import org.wildcodeschool.myblog.mapper.ArticleMapper;
+import org.wildcodeschool.myblog.model.*;
+import org.wildcodeschool.myblog.repository.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleMapper articleMapper;
+    private final CategoryRepository categoryRepository;
+    private final ImageRepository imageRepository;
+    private final AuthorRepository authorRepository;
+    private final ArticleAuthorRepository articleAuthorRepository;
+
+    public ArticleService(
+            ArticleRepository articleRepository,
+            ArticleMapper articleMapper,
+            CategoryRepository categoryRepository,
+            ImageRepository imageRepository,
+            AuthorRepository authorRepository,
+            ArticleAuthorRepository articleAuthorRepository) {
+        this.articleRepository = articleRepository;
+        this.articleMapper = articleMapper;
+        this.categoryRepository = categoryRepository;
+        this.imageRepository = imageRepository;
+        this.authorRepository = authorRepository;
+        this.articleAuthorRepository = articleAuthorRepository;
+    }
+
+
+    public List<ArticleDTO> getAllArticles() {
+        return  articleRepository.findAll()
+                .stream()
+                .map(articleMapper::toDTO).toList();
+
+    }
+
+
+    public ArticleDTO getArticleById(Long id) {
+        Article article = articleRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("L'article avec l'id " + id + " n'a pas été trouvé"));
+        return articleMapper.toDTO(article);
+    }
+
+
+    public ArticleDTO createArticle(Article article) {
+        LocalDateTime now = LocalDateTime.now();
+        article.setCreatedAt(now);
+        article.setUpdatedAt(now);
+
+        if (article.getCategory() != null) {
+            Category category = categoryRepository.findById(article.getCategory().getId()).orElseThrow(() -> new ResourceNotFoundException("La catégorie avec l'id " + article.getCategory().getId() + " n'a pas été trouvée"));
+            article.setCategory(category);
+        }
+
+        if (article.getImages() != null && !article.getImages().isEmpty()) {
+            List<Image> validImages = new ArrayList<>();
+            for (Image image : article.getImages()) {
+                if (image.getId() != null) {
+                    // Vérification des images existantes
+                    Image existingImage = imageRepository.findById(image.getId()).orElse(null);
+                    if (existingImage != null) {
+                        validImages.add(existingImage);
+                    } else {
+                        return null;
+                    }
+                } else {
+                    // Création de nouvelles images
+                    Image savedImage = imageRepository.save(image);
+                    validImages.add(savedImage);
+                }
+            }
+            article.setImages(validImages);
+        }
+        Article savedArticle = articleRepository.save(article);
+        if (article.getArticleAuthors() != null) {
+            for (ArticleAuthor articleAuthor : article.getArticleAuthors()) {
+                Author author = articleAuthor.getAuthor();
+                author = authorRepository.findById(author.getId()).orElse(null);
+                if (author == null) {
+                    return null;
+                }
+
+                articleAuthor.setAuthor(author);
+                articleAuthor.setArticle(savedArticle);
+                articleAuthor.setContribution(articleAuthor.getContribution());
+
+                articleAuthorRepository.save(articleAuthor);
+            }
+        }
+        return articleMapper.toDTO(savedArticle);
+    }
+
+
+    public ArticleDTO updateArticle(Long id,Article articleDetails) {
+        Article article = articleRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("L'article avec l'id " + id + " n'a pas été trouvé"));
+        article.setTitle(articleDetails.getTitle());
+        article.setContent(articleDetails.getContent());
+        article.setUpdatedAt(LocalDateTime.now());
+
+        if (articleDetails.getCategory() != null) {
+            Category category = categoryRepository.findById(articleDetails.getCategory().getId()).orElseThrow(() -> new ResourceNotFoundException("L'article avec l'id " + id + " n'a pas été trouvé"));
+
+            article.setCategory(category);
+        }
+
+        if (articleDetails.getImages() != null) {
+            List<Image> validImages = new ArrayList<>();
+            for (Image image : articleDetails.getImages()) {
+                if (image.getId() != null) {
+                    // Vérification des images existantes
+                    Image existingImage = imageRepository.findById(image.getId()).orElseThrow(() -> new ResourceNotFoundException("L'article avec l'id " + id + " n'a pas été trouvé"));
+                    if (existingImage != null) {
+                        validImages.add(existingImage);
+                    } else {
+                        return null; // Image non trouvée, retour d'une erreur
+                    }
+                } else {
+                    // Création de nouvelles images
+                    Image savedImage = imageRepository.save(image);
+                    validImages.add(savedImage);
+                }
+            }
+            // Mettre à jour la liste des images associées
+            article.setImages(validImages);
+        } else {
+            // Si aucune image n'est fournie, on nettoie la liste des images associées
+            article.getImages().clear();
+        }
+        if (articleDetails.getArticleAuthors() != null) {
+            articleAuthorRepository.deleteAll(article.getArticleAuthors());
+
+            List<ArticleAuthor> updatedArticleAuthors = new ArrayList<>();
+
+            for (ArticleAuthor articleAuthorDetails : articleDetails.getArticleAuthors()) {
+                Author author = articleAuthorDetails.getAuthor();
+                author = authorRepository.findById(author.getId()).orElseThrow(() -> new ResourceNotFoundException("L'article avec l'id " + id + " n'a pas été trouvé"));
+
+                ArticleAuthor newArticleAuthor = new ArticleAuthor();
+                newArticleAuthor.setAuthor(author);
+                newArticleAuthor.setArticle(article);
+                newArticleAuthor.setContribution(articleAuthorDetails.getContribution());
+
+                updatedArticleAuthors.add(newArticleAuthor);
+            }
+
+            articleAuthorRepository.saveAll(updatedArticleAuthors);
+
+            article.setArticleAuthors(updatedArticleAuthors);
+        }
+
+
+        Article updatedArticle = articleRepository.save(article);
+        return articleMapper.toDTO(updatedArticle);
+    }
+
+
+    public boolean deleteArticle(Long id) {
+        Article article = articleRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("L'article avec l'id " + id + " n'a pas été trouvé"));
+        articleAuthorRepository.deleteAll(article.getArticleAuthors());
+        articleRepository.delete(article);
+        return true;
+    }
+
+
+    public List<ArticleDTO> getArticlesByTitle(String searchTerms){
+        List<Article> articles = articleRepository.findByTitle(searchTerms);
+        if (articles.isEmpty()) {
+            return null;
+        }
+        return articles.stream().map(articleMapper::toDTO).toList();
+    }
+
+
+    public List<ArticleDTO> getArticlesByContent(String keyword){
+        List<Article> articlesByContent = articleRepository.findByContentContainingIgnoreCase(keyword);
+        if (articlesByContent.isEmpty()) {
+            return null;
+        }
+        return articlesByContent.stream().map(articleMapper::toDTO).toList();
+    }
+
+
+    public List<ArticleDTO> getArticlesByCreatedAfter(LocalDateTime createdAfter){
+        List<Article> articlesByCreatedAfter = articleRepository.findByCreatedAtAfter(createdAfter);
+        if (articlesByCreatedAfter.isEmpty()) {
+            return null;
+        }
+        return articlesByCreatedAfter.stream().map(articleMapper::toDTO).toList();
+    }
+
+
+    public List<Article> getFiveLastArticles(){
+        List<Article> fiveLastArticles = articleRepository.findByOrderByCreatedAtDesc(Limit.of(5));
+        if (fiveLastArticles.isEmpty()) {
+            return null;
+        }
+        return fiveLastArticles;
+    }
+
+}
